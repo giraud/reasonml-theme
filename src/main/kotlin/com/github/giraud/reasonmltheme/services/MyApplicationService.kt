9617634faa43250b6ef3c607a56b7f66557ecf3f
GitHub Action <action@github.com>
2021-11-25 11:09:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/giraud/reasonmltheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/giraud/reasonmltheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.giraud.reasonmltheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.giraud.reasonmltheme.MyBundle
 
 class MyApplicationService {
 
